bc77571d83684dea4ae62e25671bc93a3f989375
Antonio S. Cofiño <cofinoa@gmail.com>
2026-06-02 14:42:36 +0200

    Fix path to meta.yaml in build script for upstream validation

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,5 +2,5 @@
 export DISABLE_AUTOBREW=1
 conda-recipe-upstream-validator \
   --description "$SRC_DIR/DESCRIPTION" \
-  --meta-yaml recipe/meta.yaml || exit 1
+  --meta-yaml "$RECIPE_DIR/meta.yaml" || exit 1
 ${R} CMD INSTALL --build . ${R_ARGS}
