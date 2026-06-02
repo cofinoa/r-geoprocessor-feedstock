27408320e1145986c82995d0e4400f2b8b381127
Antonio S. Cofiño <cofinoa@gmail.com>
2026-06-02 14:25:39 +0200

    Update build scripts and metadata for improved validation and versioning

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-export DISABLE_AUTOBREW=1
+export DISABLE_AUTOBREW=1ç
+conda-recipe-upstream-validator \
+  --description "$SRC_DIR/DESCRIPTION" \
+  --meta-yaml recipe/meta.yaml || exit 1
 ${R} CMD INSTALL --build . ${R_ARGS}
