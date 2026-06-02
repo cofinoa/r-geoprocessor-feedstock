5c8ebfd4b8b8733406d47e1cd4948cada1216f57
Antonio S. Cofiño <cofinoa@gmail.com>
2026-06-02 14:30:55 +0200

    Fix typo in build script by removing extraneous character from DISABLE_AUTOBREW export

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export DISABLE_AUTOBREW=1ç
+export DISABLE_AUTOBREW=1
 conda-recipe-upstream-validator \
   --description "$SRC_DIR/DESCRIPTION" \
   --meta-yaml recipe/meta.yaml || exit 1
